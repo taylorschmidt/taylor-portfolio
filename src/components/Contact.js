1ec2a67cf62a001946fa2110dd009a2cd8765920
taylorschmidt <taylorhillschmidt@gmail.com>
2021-01-30 09:44:38 -0700

    added mobile contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,60 +1,76 @@
-import React from 'react';
-import emailjs from 'emailjs-com';
+import React from "react";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
-
   function sendEmail(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 
-    emailjs.sendForm('service_offdx2r', 'template_pgmt5xj', e.target, 'user_yeDGiqKuNO4scyEGPOLB5')
-      .then((result) => {
-          window.location.reload()
-      }, (error) => {
+    emailjs
+      .sendForm(
+        "service_offdx2r",
+        "template_pgmt5xj",
+        e.target,
+        "user_yeDGiqKuNO4scyEGPOLB5"
+      )
+      .then(
+        (result) => {
+          window.location.reload();
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   }
 
   return (
-  <>
-<div className="container center-align">
-<h1 className='page-title'>let's chat</h1>
-    <div class="row center-align">
-    <form class="contact-form sm" onSubmit={sendEmail}>
-        <input type="hidden" name="contact_number" />
-      <div class=" center-align row">
-        <div class="center-align input-field col s12">
-          <input type="text" name="from_name" class="validate"/>
-          <label for="first_name">Name</label>
+    <>
+      <div className="container center-align">
+        <div className="row valign-wrapper center-align">
+          <div className="col s12 m6 l6 chat-box valign-wrapper center-align">
+            <h1 className="page-title">let's chat</h1>
+          </div>
+          <div className="col s12 m6 l6">
+            <form class="contact-form sm center-align " onSubmit={sendEmail}>
+              <input type="hidden" name="contact_number" />
+              <div class=" center-align row">
+                <div class="center-align input-field col s12">
+                  <input
+                    className="center-align"
+                    type="text"
+                    name="from_name"
+                    class="validate"
+                  />
+                  <label className="center-align" for="first_name">
+                    Name
+                  </label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input
+                    type="email"
+                    name="from_email"
+                    id="email"
+                    class="validate"
+                  />
+                  <label for="password">Email</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea
+                    id="textarea1"
+                    className="materialize-textarea"
+                    name="message"
+                  ></textarea>
+                  <label for="textarea1">Message</label>
+                </div>
+              </div>
+              <input className="buttons btn" type="submit" value="Send" />
+            </form>
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input type="email" name="from_email" id="email" class="validate"/>
-          <label for="password">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-            <textarea id="textarea1" className="materialize-textarea" name="message"></textarea>
-            <label for="textarea1">Message</label>
-        </div>
-        </div>
-        <input className='buttons btn' type="submit" value="Send" />
-    </form>
-    
-  </div>
-      
-    {/* <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="from_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input className='buttons btn' type="submit" value="Send" />
-    </form> */}
-    </div>
     </>
   );
 }
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,44 +1,88 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { FaLinkedin, FaGithub, FaHtml5, FaReact, FaNodeJs, FaCss3Alt, FaPython, FaGit, FaDatabase, FaBootstrap } from 'react-icons/fa';
-import {SiJavascript, SiMongodb, SiPostgresql, SiPostman, SiFlask} from 'react-icons/si'
-import {AiOutlineMail} from 'react-icons/ai'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaHtml5,
+  FaReact,
+  FaNodeJs,
+  FaCss3Alt,
+  FaPython,
+  FaGit,
+  FaDatabase,
+  FaBootstrap,
+} from "react-icons/fa";
+import {
+  SiJavascript,
+  SiMongodb,
+  SiPostgresql,
+  SiPostman,
+  SiFlask,
+} from "react-icons/si";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Layout = (props) => {
-    return(
-        <div className='main'>
-            <nav>
-                <div className="nav-wrapper">
-                    <ul id="nav-mobile"
-                    // hide-on-med-and-down
-                     className="right ">
-                        <li><Link to='/about'>ABOUT</Link></li>
-                        <li><Link to='/projects'>PROJECTS</Link></li>
-                        <li><Link to='/contact'>CONTACT</Link></li>
-                    </ul>
-                    <ul id="nav-mobile"
-                    // hide-on-med-and-down
-                     className="left ">
-                        <li><Link to='/'>HOME</Link></li>
-                    </ul>
-                </div>
-            </nav>
-            <div>
-                {/* render anything that is passed - in this case, our routes */}
-                {props.children}
-            </div>
-<footer className="page-footer">
-   <div className="container">
-       <div className="footer-container">
-       <a href="https://github.com/taylorschmidt"> <FaGithub size="2em" color="white" /></a>
-       <a href="https://www.linkedin.com/in/taylorhschmidt/"> <FaLinkedin size="2em" color="white" /></a>
-       <a href="mailto:taylorschmidtcodes@gmail.com"> <AiOutlineMail size="2em" color="white" /></a>
-       <div>© Taylor Schmidt 2021</div>
-       </div>
-   </div>
-</footer>
+  return (
+    <body className="body">
+    {/* <div className="main"> */}
+      <header>
+        <nav>
+          <div className="nav-wrapper">
+            <ul
+              id="nav-mobile"
+              // hide-on-med-and-down
+              className="right "
+            >
+              <li>
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li>
+                <Link to="/projects">PROJECTS</Link>
+              </li>
+              <li>
+                <Link to="/contact">CONTACT</Link>
+              </li>
+            </ul>
+            <ul
+              id="nav-mobile"
+              // hide-on-med-and-down
+              className="left "
+            >
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div>
+          {/* render anything that is passed - in this case, our routes */}
+          {props.children}
         </div>
-    )
-}
+      </main>
+      <footer className="page-footer">
+        <div className="container">
+          <div className="footer-container">
+            <a href="https://github.com/taylorschmidt">
+              {" "}
+              <FaGithub size="2em" color="white" />
+            </a>
+            <a href="https://www.linkedin.com/in/taylorhschmidt/">
+              {" "}
+              <FaLinkedin size="2em" color="white" />
+            </a>
+            <a href="mailto:taylorschmidtcodes@gmail.com">
+              {" "}
+              <AiOutlineMail size="2em" color="white" />
+            </a>
+            <div>© Taylor Schmidt 2021</div>
+          </div>
+        </div>
+      </footer>
+    {/* </div> */}
+</body>
+  );
+};
 
-export default Layout
+export default Layout;
